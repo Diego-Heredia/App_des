3c4b0f66a119ef0b1b814e4f0505b75ff69db461
Diego-Heredia <64281213+Diego-Heredia@users.noreply.github.com>
2022-02-26 04:00:40 -0600

    Primeros archivos

---
+++ assets/js/main.js
@@ -0,0 +1,546 @@
+/**
+* Template Name: Arsha - v4.7.1
+* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate  glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
+
+
+var state = [];
+
+function setDefaultState() {
+  var id = generateID();
+  var baseState = {};
+  baseState[id] = {
+    status: "new",
+    id: id,
+    title: "This site uses 🍪to keep track of your tasks"
+  };
+  syncState(baseState);
+}
+
+function generateID() {
+  var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  return randLetter + Date.now();
+}
+
+function pushToState(title, status, id) {
+  var baseState = getState();
+  baseState[id] = { id: id, title: title, status: status };
+  syncState(baseState);
+}
+
+function setToDone(id) {
+  var baseState = getState();
+  if (baseState[id].status === 'new') {
+    baseState[id].status = 'done'
+  } else {
+    baseState[id].status =  'new';
+  }
+
+  syncState(baseState);
+}
+
+function deleteTodo(id) {
+  console.log(id)
+  var baseState = getState();
+  delete baseState[id]
+  syncState(baseState)
+}
+
+function resetState() {
+  localStorage.setItem("state", null);
+}
+
+function syncState(state) {
+  localStorage.setItem("state", JSON.stringify(state));
+}
+
+function getState() {
+  return JSON.parse(localStorage.getItem("state"));
+}
+
+function addItem(text, status, id, noUpdate) {
+  var id = id ? id : generateID();
+  var c = status === "done" ? "danger" : "";
+  var item =
+    '<li data-id="' +
+    id +
+    '" class="animated flipInX ' +
+    c +
+    '"><div class="checkbox"><span class="close"><i class="fa fa-times"></i></span><label><span class="checkbox-mask"></span><input type="checkbox" />' +
+    text +
+    "</label></div></li>";
+
+  var isError = $(".form-control").hasClass("hidden");
+
+  if (text === "") {
+    $(".err")
+      .removeClass("hidden")
+      .addClass("animated bounceIn");
+  } else {
+    $(".err").addClass("hidden");
+    $(".todo-list").append(item);
+  }
+
+  $(".refresh").removeClass("hidden");
+
+  $(".no-items").addClass("hidden");
+
+  $(".form-control")
+    .val("")
+    .attr("placeholder", "✍️ Add item...");
+  setTimeout(function() {
+    $(".todo-list li").removeClass("animated flipInX");
+  }, 500);
+
+  if (!noUpdate) {
+    pushToState(text, "new", id);
+  }
+}
+
+function refresh() {
+  $(".todo-list li").each(function(i) {
+    $(this)
+      .delay(70 * i)
+      .queue(function() {
+        $(this).addClass("animated bounceOutLeft");
+        $(this).dequeue();
+      });
+  });
+
+  setTimeout(function() {
+    $(".todo-list li").remove();
+    $(".no-items").removeClass("hidden");
+    $(".err").addClass("hidden");
+  }, 800);
+}
+
+$(function() {
+  var err = $(".err"),
+    formControl = $(".form-control"),
+    isError = formControl.hasClass("hidden");
+
+  if (!isError) {
+    formControl.blur(function() {
+      err.addClass("hidden");
+    });
+  }
+
+  $(".add-btn").on("click", function() {
+    var itemVal = $(".form-control").val();
+    addItem(itemVal);
+    formControl.focus();
+  });
+
+  $(".refresh").on("click", refresh);
+
+  $(".todo-list").on("click", 'input[type="checkbox"]', function() {
+    var li = $(this)
+      .parent()
+      .parent()
+      .parent();
+    li.toggleClass("danger");
+    li.toggleClass("animated flipInX");
+
+    setToDone(li.data().id);
+
+    setTimeout(function() {
+      li.removeClass("animated flipInX");
+    }, 500);
+  });
+
+  $(".todo-list").on("click", ".close", function() {
+    var box = $(this)
+      .parent()
+      .parent();
+
+    if ($(".todo-list li").length == 1) {
+      box.removeClass("animated flipInX").addClass("animated                bounceOutLeft");
+      setTimeout(function() {
+        box.remove();
+        $(".no-items").removeClass("hidden");
+        $(".refresh").addClass("hidden");
+      }, 500);
+    } else {
+      box.removeClass("animated flipInX").addClass("animated bounceOutLeft");
+      setTimeout(function() {
+        box.remove();
+      }, 500);
+    }
+
+    deleteTodo(box.data().id)
+  });
+
+  $(".form-control").keypress(function(e) {
+    if (e.which == 13) {
+      var itemVal = $(".form-control").val();
+      addItem(itemVal);
+    }
+  });
+  $(".todo-list").sortable();
+  $(".todo-list").disableSelection();
+});
+
+var todayContainer = document.querySelector(".today");
+
+
+var d = new Date();
+
+
+var weekday = new Array(7);
+weekday[0] = " Domingo 🖖";
+weekday[1] = " Lunes 💪😀";
+weekday[2] = " Martes 😜";
+weekday[3] = " Miercoles 😌☕️";
+weekday[4] = " Jueves 🤗";
+weekday[5] = " Viernes 🍻";
+weekday[6] = " Sabado 🍻";
+
+var n = weekday[d.getDay()];
+
+var randomWordArray = Array(
+  "Intercambios del día"
+);
+
+var randomWord =
+  randomWordArray[Math.floor(Math.random() * randomWordArray.length)];
+
+
+todayContainer.innerHTML = randomWord + n;
+
+$(document).ready(function() {
+  var state = getState();
+
+  if (!state) {
+    setDefaultState();
+    state = getState();
+  }
+
+  Object.keys(state).forEach(function(todoKey) {
+    var todo = state[todoKey];
+    addItem(todo.title, todo.status, todo.id, true);
+  });
+
+  var mins, secs, update;
+
+  init();
+  function init() {
+    (mins = 25), (secs = 59);
+  }
+
+
+  set();
+  function set() {
+    $(".mins").text(mins);
+  }
+
+
+  $("#start").on("click", start_timer);
+  $("#reset").on("click", reset);
+  $("#inc").on("click", inc);
+  $("#dec").on("click", dec);
+
+  function start_timer() {
+
+    set();
+
+    $(".dis").attr("disabled", true);
+
+    $(".mins").text(--mins);
+    $(".separator").text(":");
+    update_timer();
+
+    update = setInterval(update_timer, 1000);
+  }
+
+  function update_timer() {
+    $(".secs").text(secs);
+    --secs;
+    if (mins == 0 && secs < 0) {
+      reset();
+    } else if (secs < 0 && mins > 0) {
+      secs = 59;
+      --mins;
+      $(".mins").text(mins);
+    }
+  }
+
+
+  function reset() {
+    clearInterval(update);
+    $(".secs").text("");
+    $(".separator").text("");
+    init();
+    $(".mins").text(mins);
+    $(".dis").attr("disabled", false);
+  }
+
+
+  function inc() {
+    mins++;
+    $(".mins").text(mins);
+  }
+
+
+  function dec() {
+    if (mins > 1) {
+      mins--;
+      $(".mins").text(mins);
+    } else {
+      alert("This is the minimum limit.");
+    }
+  }
+}); 
+
